27271d864bfe1afb4f81760b3975d12d1d818a65
admin <admin@192.168.0.21>
2019-06-15 20:28:27 +0530

    first commit

---
+++ src/main/java/com/st/entity/Product.java
@@ -0,0 +1,25 @@
+package com.st.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Product {
+		@Id
+		@NonNull
+		private Integer prodId;
+		@NonNull
+		private String comName;
+		@NonNull
+		private String prodName;
+		@NonNull
+		private String prodCost;
+}
